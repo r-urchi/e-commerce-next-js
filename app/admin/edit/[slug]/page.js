b403222a6b71e901d5ba1a6d745d17f51b87d47e
Ramon Urchipia <ramon.urchipia@infracommerce.lat>
2023-12-15 16:54:36 -0300

    test admin

--- app/admin/edit/[slug]/page.js
+++ app/admin/edit/[slug]/page.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import EditForm from '@/app/components/admin/EditForm'
+import { items } from '@/mockData'
 
 const EditPage = async ({ params }) => {
 
     const { slug } = params
 
-    const item = await fetch(`${process.env.NEXT_PUBLIC_VERCEL_URL}/api/product/${slug}`, {
-        cache: 'no-cache',
+    // const item = await fetch(`${process.env.NEXT_PUBLIC_VERCEL_URL}/api/product/${slug}`, {
+    //     cache: 'no-cache',
         
-    })
-        .then(res => res.json())
+    // })
+    //     .then(res => res.json())
 
     return (
         <div>
-            <EditForm item={item} />
+            <EditForm item={items[0]} />
         </div>
     )
 }
